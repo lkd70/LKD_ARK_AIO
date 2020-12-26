0a9285c9d929626574cb9e5b9f7077b2cc4993e4
LKD70 <lkdornan70@gmail.com>
2020-12-26 18:05:22 +0000

    Self contained compiler, added compression

---
+++ compile.ps1
@@ -0,0 +1,7 @@
+a$Compiler = ".\Compiler\Ahk2Exe.exe"
+$BinFIle = ".\Compiler\Unicode 64-bit.bin"
+$inFile = ".\ARK_All_In_One.ahk"
+$outFile = ".\ARK_All_In_One.exe"
+$iconFile = ".\icon.ico"
+
+& $compiler  /bin $BinFIle /in $inFile /out $outFile /icon $iconFile /compress 2
